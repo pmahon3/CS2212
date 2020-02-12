e5316e2057c36e59d721c2633e07d68607c7c95b
RyanH35 <60946403+RyanH35@users.noreply.github.com>
2020-02-11 22:47:00 -0500

    Update HighQuantityProxy.java

--- Project/cs2212-project-documented/src/ca/uwo/proxies/HighQuantityProxy.java
+++ Project/cs2212-project-documented/src/ca/uwo/proxies/HighQuantityProxy.java
@@ -9,20 +9,21 @@ public class HighQuantityProxy extends Proxy{
 	
 	protected Proxy next;
 	
+	//HighQuantityProxy is the last object in the chain of responsibility, so next is set to null
 	public HighQuantityProxy() {
 		
 		next = null;
 	}
 
-	@Override
+	//Handles all orders with more than 10 items
 	public void placeOrder(Map<String, Integer> orderDetails, Buyer buyer) throws Throwable {
-		// TODO Auto-generated method stub
+		Facade facade = new Facade();
+		facade.placeOrder(orderDetails, buyer);
 		
 	}
 
-	@Override
+	//HighQuantityProxy will not handle any restock requests
 	public void restock(Map<String, Integer> restockDetails, Supplier supplier) {
-		// TODO Auto-generated method stub
 		
 	}
 
