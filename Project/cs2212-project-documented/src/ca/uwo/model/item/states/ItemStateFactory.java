12697572a98a2dc7500a220d2ce0c3a9600e1384
NohlEgan <39224642+NohlEgan@users.noreply.github.com>
2020-03-20 17:00:32 -0400

    Update ItemStateFactory.java
    - changed parameter of create function
    - changed case statements to if/else statements

--- Project/cs2212-project-documented/src/ca/uwo/model/item/states/ItemStateFactory.java
+++ Project/cs2212-project-documented/src/ca/uwo/model/item/states/ItemStateFactory.java
@@ -3,14 +3,15 @@ package ca.uwo.model.item.states;
 //This class is responsible for creating different states (using Factory design pattern)
 
 public class ItemStateFactory {
-	public static ItemState create(String type) {
-		switch(type) {
-		case "In Stock":
-			return new inStockState();
-		"Low Stock":
-			return new lowStockState();
-		"Out of Stock":
-			return new outOfStockState();
+	public static ItemState create(int quantity) {
+		if (quantity > 10) {
+			return new InStockState();
+		}
+		else if (quantity < 10 && quantity > 0) {
+			return new LowStockState();
+		}
+		else {
+			return new OutOfStockState();
 		}
 	}
 }
