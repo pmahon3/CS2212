f865efe1f2ca0ff4541fead2ef8d78bf894e4c6b
NohlEgan <39224642+NohlEgan@users.noreply.github.com>
2020-04-02 19:27:56 -0400

    Update IndividualPricingStrategy2.java

--- Project/cs2212-project-documented/src/ca/uwo/pricingStrategies/individual/IndividualPricingStrategy2.java
+++ Project/cs2212-project-documented/src/ca/uwo/pricingStrategies/individual/IndividualPricingStrategy2.java
@@ -3,6 +3,16 @@ package ca.uwo.pricingStrategies.individual;
 public class IndividualPricingStrategy2 implements IndividualPricingStrategy {
 	public double calculate(int quantity, double price) {
 		double discount = quantity * price * 0.10;
-		return (quantity * price) - discount;
+		//adds 10% to small orders
+		if(quantity < 10) {
+			return (quantity * price) + discount;
+		}
+		//subtracts 10% to large orders
+		else if (quantity > 100) {
+			return (quantity * price) - discount;
+		}
+		else {
+			return (quantity * price);
+		}
 	}
 }
